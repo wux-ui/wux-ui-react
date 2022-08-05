4710fba745a0f3318b293dead8451aa4534ef197
Slouchwind <2241051890@qq.com>
2022-08-05 18:15:29 +0800

    🍧 Button

--- index.d.ts
+++ index.d.ts
@@ -1,22 +1,55 @@
 interface AlertElements {
+    /**Primary alert */
     Primary: (props: any) => JSX.Element;
+    /**Secondary alert */
     Secondary: (props: any) => JSX.Element;
+    /**Success alert */
     Success: (props: any) => JSX.Element;
+    /**Info alert */
     Info: (props: any) => JSX.Element;
+    /**Warning alert */
     Warning: (props: any) => JSX.Element;
+    /**Error alert */
     Error: (props: any) => JSX.Element;
+    /**Light alert */
     Light: (props: any) => JSX.Element;
+    /**Icon alert */
     Icon: (props: any) => JSX.Element;
 }
 
+
+
 interface HeaderProps {
+    /**a string show as title  */
     title: string;
-    option: Array[][];
+    /**an Array with Array(string)  
+     * `string[0]` is the link title  
+     * `string[1]` is the link */
+    option?: string[][];
 }
 
 const Wux: {
+    /**Basic element */
     Basic: (props: any) => JSX.Element;
+    /**Header element  
+     *   
+     * The param `props` should be an object with `title` and `option` as properties  
+     * `title` is a string show as title  
+     * `option` is an Array with Array(string), `string[0]` is the link title, `string[1]` is the link  
+     *   
+     * This is a Header element template :
+     * ```js
+     * <Wux.Header
+     *     title="WuX-UI Debug Page"
+     *     option={[
+     *         ['Offical Page', 'https://wux-ui.tk/'],
+     *         ['Github', 'https://github.com/wux-ui/wux-ui']
+     *     ]}
+     * />
+     * ```
+     */
     Header: (props: HeaderProps) => JSX.Element,
+    /**Col element */
     Col: (props: any) => JSX.Element;
     /**Alert elements */
     Alert: AlertElements;
--- index.js
+++ index.js
@@ -1,23 +1,22 @@
 import React from "react"
 import "./import.css"
 
-class Header extends React.Component {
-    constructor(props) {
-        super(props);
-    }
-
-    render() {
-        var optionGroup = this.props.option.map(v => <a class="wux-header-option" href={v[1]}>{v[0]}</a>);
-        return <nav class="wux-header wux-header-fixed">
-            <span class="wux-header-title">{this.props.title}</span>
-            <span class="wux-header-option-group">{optionGroup}</span>
-        </nav>
-    }
-}
-
 const Wux = {
     Basic: props => <div className="wux-header-fixed-margin">{props.children}</div>,
-    Header: Header,
+    Header: props => {
+        var optionGroup = props.option.map((v, i) =>
+            <a
+                className="wux-header-option"
+                href={v[1]}
+                key={i}>
+                {v[0]}
+            </a>
+        );
+        return <nav className="wux-header wux-header-fixed">
+            <span className="wux-header-title">{props.title}</span>
+            <span className="wux-header-option-group">{optionGroup}</span>
+        </nav>
+    },
     Col: props => <div className="wux-col">{props.children}</div>,
     Alert: {
         Primary: props => <div className="wux-alert wux-alert-primary">{props.children}</div>,
