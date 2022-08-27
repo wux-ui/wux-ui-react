c148712b260a7e4dea62119b89f5703c13459c40
Slouchwind <2241051890@qq.com>
2022-08-27 18:22:34 +0800

    .

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
-import "./import.css";
+import React, { useState } from 'react';
+import './import.css';
 
 function getClass(defaultValue, type, noDefault = false) {
     if (type) {
