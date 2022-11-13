bed70d030c767bcf477af571dcee0e0fd9e778eb
Slouchwind <2241051890@qq.com>
2022-11-13 16:04:31 +0800

    var => let

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ import './import.css';
 
 function getClass(defaultValue, type, noDefault = false) {
     if (type) {
-        var typeType = type.constructor.toString().split(' ')[1].split('(')[0];
+        let typeType = type.constructor.toString().split(' ')[1].split('(')[0];
         switch (typeType) {
             case 'String':
                 return `${noDefault ? '' : defaultValue + ' '}${defaultValue}-${type}`;
             case 'Array':
-                var className = noDefault ? '' : defaultValue;
+                let className = noDefault ? '' : defaultValue;
                 type.forEach(v => className += ` ${defaultValue}-${v}`);
                 return className;
             default:
@@ -21,10 +21,10 @@ function getClass(defaultValue, type, noDefault = false) {
 const WuX = {
     HeaderMargin: props => <div className="wux-header-fixed-margin">{props.children}</div>,
     Header: ({ type, option, title, small, ...otherProps }) => {
-        var optionGroup = option.forEach((v, i) => {
+        let optionGroup = option.map((v, i) => {
             if (v.constructor.toString().split(' ')[1].split('(')[0] === 'Object') {
                 if (v.$$typeof === (<p />).$$typeof) {
-                    var Type = v.type;
+                    let Type = v.type;
                     return <Type key={i} {...v.props} />;
                 }
                 else {
@@ -37,6 +37,7 @@ const WuX = {
                     </a>;
                 }
             }
+            else return <></>;
         });
         return <nav className={type ? getClass('wux-header', type) : 'wux-header'} {...otherProps}>
             <span className="wux-header-title">{title}</span>
@@ -46,12 +47,12 @@ const WuX = {
     },
     Container: props => <div className="wux-container">{props.children}</div>,
     Row: props => {
-        var type = props.size.constructor.toString().split(' ')[1].split('(')[0];
+        let type = props.size.constructor.toString().split(' ')[1].split('(')[0];
         switch (type) {
             case 'String':
                 return <div className={`wux-row-${props.size}-${props.number}`}>{props.children}</div>
             case 'Array':
-                var className = '';
+                let className = '';
                 props.size.forEach((_, i) => className += ` wux-row-${props.size[i]}-${props.number[i]}`);
                 return <div className={className.slice(1)} disabled={props.disabled}>{props.children}</div>
             default:
@@ -66,12 +67,12 @@ const WuX = {
     Button: ({ WuXType, ...otherProps }) => <button className={getClass('wux-btn', WuXType)}  {...otherProps} />,
     ButtonGroup: ({ type, ...otherProps }) => <div className={getClass('wux-btn-group', type)} {...otherProps} />,
     Breadcrumb: props => {
-        var type = props.item.constructor.toString().split(' ')[1].split('(')[0];
+        let type = props.item.constructor.toString().split(' ')[1].split('(')[0];
         switch (type) {
             case 'String':
                 return <ul className="wux-breadcrumb"><li className="wux-breadcrumb-item">{props.item}</li></ul>
             case 'Array':
-                var breadcrumbItems = props.item.map((v, i) => <li className="wux-breadcrumb-item" key={i}>{v}</li>);
+                let breadcrumbItems = props.item.map((v, i) => <li className="wux-breadcrumb-item" key={i}>{v}</li>);
                 return <ul className="wux-breadcrumb">{breadcrumbItems}</ul>
             default:
                 break;
@@ -84,12 +85,12 @@ const WuX = {
             <div className="wux-card-footer">{footer}</div>
         </div>,
     Dialog: ({ id, header, body, cancel, footer, btn = <WuX.Button />, children, ...otherProps }) => {
-        var cancelBtn = cancel;
-        var Cancel = cancel.type;
-        var footerBtn = footer.props.children;
-        var Btn = btn.type;
-        var type = footerBtn.constructor.toString().split(' ')[1].split('(')[0];
-        var footerElement;
+        let cancelBtn = cancel;
+        let Cancel = cancel.type;
+        let footerBtn = footer.props.children;
+        let Btn = btn.type;
+        let type = footerBtn.constructor.toString().split(' ')[1].split('(')[0];
+        let footerElement;
         switch (type) {
             case 'String':
                 footerElement = footer;
@@ -119,7 +120,7 @@ const WuX = {
         </>
     },
     Dropdown: ({ menu, btn = <WuX.Button />, children, ...otherProps }) => {
-        var items = menu.map((v, i) => <li className="wux-dropdown-item" key={i}>{v}</li>);
+        let items = menu.map((v, i) => <li className="wux-dropdown-item" key={i}>{v}</li>);
         return <div className="wux-dropdown" {...otherProps}>
             <button className={`${getClass('wux-btn', btn.props.type)} wux-dropdown-trigger`} {...btn.props}>{children}</button>
             <ul className="wux-dropdown-menu">{items}</ul>
@@ -146,8 +147,8 @@ const WuX = {
         </label>
     },
     Radio: ({ suffix = <br />, children, ...otherProps }) => {
-        var Suffix = suffix.type;
-        var radios = children.map((v, i) => [
+        let Suffix = suffix.type;
+        let radios = children.map((v, i) => [
             <label key={`label-${i}`}>
                 <input className="wux-form-radios" type="radio" name="contact" {...otherProps} />
                 {v}
@@ -160,7 +161,7 @@ const WuX = {
     Select: ({ value, children, ...otherProps }) => {
         value = value || children[0];
         const [state, setState] = React.useState({ value: value });
-        var options = children.map((v, i) => <option key={i}>{v}</option>)
+        let options = children.map((v, i) => <option key={i}>{v}</option>)
         return <select
             className="wux-form-select"
             onChange={(e) => { setState(e.target.value) }}
@@ -180,7 +181,7 @@ const WuX = {
         }
     },
     Jumbotron: ({ before, title, subtitle, btn, after, ...otherProps }) => {
-        var linkBtn = btn.map((v, i) => {
+        let linkBtn = btn.map((v, i) => {
             const { type, children } = v[1].props;
             return <a href={v[0]} key={i} className={getClass('wux-btn', type)}  {...v[2]}>{children}</a>;
         })
@@ -193,11 +194,11 @@ const WuX = {
         </div>
     },
     JumbotronTitleBlod: ({ type, ...otherProps }) => {
-        var Type = type.type;
+        let Type = type.type;
         return <Type className='wux-jumbotron-title-bold' {...otherProps} />
     },
     List: ({ type, children, ...otherProps }) => {
-        var group = children.map((v, i) => <li className={getClass(`wux-list-item`, type[i])} key={i}>{v}</li>);
+        let group = children.map((v, i) => <li className={getClass(`wux-list-item`, type[i])} key={i}>{v}</li>);
         return <ul className="wux-list-group" {...otherProps}>{group}</ul>
     },
     Progress: ({ value, max, ...otherProps }) => <progress className="wux-progress" value={String(value)} max={String(max)}  {...otherProps} />,
@@ -207,11 +208,11 @@ const WuX = {
             <span className={`wux-tooltip-item wux-tooltip-item-${WuXType}`}>{text}</span>
         </button>,
     Table: ({ children, ...otherProps }) => {
-        var head = [];
+        let head = [];
         for (const key in children) {
             head.push(key);
         }
-        var body = [];
+        let body = [];
         head = head.map((v, i) => {
             if (i === 0) children[v].forEach((value, index) => {
                 body[index] = [];
@@ -230,7 +231,7 @@ const WuX = {
     Search: props => <input className="wux-search" {...props} />,
     Tab: ({ name, children, ...otherProps }) => {
         const [state, setState] = useState({ checked: "tab-0" });
-        var tabs = name.map((v, i) => [
+        let tabs = name.map((v, i) => [
             <input
                 className="wux-tab-item"
                 type="radio"
@@ -253,7 +254,7 @@ const WuX = {
         return <div className="wux-tab" {...otherProps}>{tabs}</div>
     },
     Blankslate: ({ icon, WuXTitle, subtitle, btn, ...otherProps }) => {
-        var btnGroup = btn.map((v, i) => {
+        let btnGroup = btn.map((v, i) => {
             const { type, children } = v[0].props;
             return <button key={i} className={getClass('wux-btn', type)}  {...v[1]}>{children}</button>;
         })
